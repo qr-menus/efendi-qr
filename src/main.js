ae01f2ae8aa9227ce87353892b85554c1d43bf1f
BSalikhov <bakhadirsaalikhov@mail.ru>
2021-04-29 03:51:46 +0500

    fix: image src edited

--- src/main.js
+++ src/main.js
@@ -6,12 +6,21 @@ import store from "./store";
 import VueScrollactive from "vue-scrollactive";
 import "./utils/localeFilter";
 import "./utils/clickOutside";
+import YmapPlugin from "vue-yandex-maps";
 
 Vue.config.productionTip = false;
 Vue.use(VueScrollactive);
+Vue.use(YmapPlugin, settings);
 
 new Vue({
   router,
   store,
   render: (h) => h(App),
 }).$mount("#app");
+
+const settings = {
+  apiKey: "3e7930fe-d6cb-40aa-a554-38c9f4cfb8f5",
+  lang: "ru_RU",
+  coordorder: "latlong",
+  version: "2.1",
+};
