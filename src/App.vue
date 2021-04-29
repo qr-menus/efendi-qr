08a1a00a03dad4e01374b8b6300caae4433ce2e6
Doniyor Yusupov <danielusupov97@gmail.com>
2021-04-29 18:18:35 +0500

    feat: added ymaps

--- src/App.vue
+++ src/App.vue
@@ -7,96 +7,9 @@
             <div class="flex items-center justify-between">
               <div class="flex items-center space-x-4">
                 <img class="w-16 h-16" src="./assets/logo.svg" alt="" />
-                <!-- <div>
-                  <h6 class="text-sm font-medium">
-                    <span class="font-bold text-primary">E1 -</span> (71)
-                    233-15-02
-                  </h6>
-                  <h6 class="text-sm font-medium">
-                    <span class="font-bold text-primary">E2 -</span> (95)
-                    177-09-93
-                  </h6>
-                </div> -->
               </div>
               <div class="flex items-center space-x-2">
-                <BaseModal>
-                  <template #trigger="{ open }">
-                    <button
-                      @click="open"
-                      class="flex px-2 py-1 font-medium text-gray-800 rounded-md"
-                    >
-                      <span v-if="$store.state.locale == 'ru'">О нас</span>
-                      <span v-else>About us</span>
-                    </button>
-                  </template>
-                  <template #default="{ close }">
-                    <div class="py-4">
-                      <h3 class="px-4 mb-2 text-lg font-bold">
-                        Контактные данные
-                      </h3>
-                      <hr />
-                      <div class="px-4 mt-4 mb-2 bg-white">
-                        <h4 class="font-medium">Efendi Restaurant - 1</h4>
-                        <div class="flex items-center mt-1 mb-2 text-gray-800">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-5 h-5 mr-2 text-primary"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
-                            />
-                          </svg>
-                          (71) 233-15-02
-                        </div>
-                      </div>
-                      <div id="map" style="width: 450px; height: 270px"></div>
-                      <div class="px-4 mt-4 mb-2 bg-white">
-                        <h4 class="font-medium">Efendi Restaurant - 2</h4>
-                        <div class="flex items-center mt-1 mb-2 text-gray-800">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="w-5 h-5 mr-2 text-primary"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
-                            />
-                          </svg>
-                          (95) 177-09-93
-                        </div>
-                      </div>
-                      <div id="map-2" style="width: 450px; height: 270px"></div>
-                      <div class="mt-4 text-center">
-                        Сделано с любовью
-                        <a
-                          href="http://qrmenus.uz/"
-                          target="_blank"
-                          class="font-bold text-primary"
-                          >qrmenus.uz</a
-                        >
-                      </div>
-                      <button @click="close" class="absolute top-3 right-3">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="w-6 h-6"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </template>
-                </BaseModal>
+                <AboutUs />
                 <LanguageDropdown :items="langOptions">
                   <template #trigger="{ click }">
                     <button
@@ -127,60 +40,6 @@
                     </button>
                   </template>
                 </LanguageDropdown>
-                <!-- <SlideOver>
-                  <template #trigger="{ onClick }">
-                    <button
-                      @click.stop="onClick"
-                      class="relative flex items-center bg-white rounded-md text-primary hover:text-red-500 focus:outline-none"
-                    >
-                      <span class="sr-only">Close panel</span>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-8 w-8"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                        />
-                      </svg>
-                    </button>
-                  </template>
-
-                  <template #default="{ close }">
-                    <div
-                      v-if="!favourites.length"
-                      class="flex flex-col h-full items-center justify-center"
-                    >
-                      <img
-                        src="/images/favourites.svg"
-                        class="w-1/2 object-cover mb-5"
-                      />
-                      <h3 class="text-xl font-bold text-gray-700 mb-3">
-                        Добавьте блюдо из меню
-                      </h3>
-                      <button
-                        @click="close"
-                        class="bg-yellow-400 text-sm font-semibold text-gray-900 px-3 py-2 rounded hover:bg-yellow-300 focus:outline-none"
-                      >
-                        Перейти в меню
-                      </button>
-                    </div>
-                    <div v-else class="flex flex-col space-y-3 px-2">
-                      <FavouriteItem
-                        v-for="(product, $productIndex) in favourites"
-                        :key="`product-${$productIndex}`"
-                        :id="$productIndex + 1"
-                        :product="product"
-                      />
-                    </div>
-                  </template>
-                </SlideOver> -->
               </div>
             </div>
           </div>
@@ -245,7 +104,7 @@
 <script>
 import LanguageDropdown from "./components/LanguageDropdown.vue";
 import FoodCard from "./components/FoodCard.vue";
-import BaseModal from "./components/BaseModal.vue";
+import AboutUs from "./components/AboutUs.vue";
 import FoodCart from "./components/FoodCart.vue";
 import { mapState } from "vuex";
 
@@ -253,7 +112,7 @@ export default {
   components: {
     LanguageDropdown,
     FoodCard,
-    BaseModal,
+    AboutUs,
     FoodCart,
   },
   computed: {
